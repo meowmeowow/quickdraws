90899fabff76b8f9b8d5c84c24a0a66f023bb96a
Scott Hassan <hassan@willowmail.com>
2024-05-13 10:27:11 -0700

    formating

--- project/main.py
+++ project/main.py
@@ -23,21 +23,6 @@ UPLOAD_FOLDER  = './project/static/photos'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 #main.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
-
-
-
-if 0:
-  file_names = list()
-  for root, dirc, files in os.walk(UPLOAD_FOLDER, topdown=True):
-
-    for FileName in files:
-      file_names.append(FileName)
-      
-start = 0
-
-
-
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -48,7 +33,6 @@ def index():
 
 @main.route('/',methods=['POST'])
 def index_post():
-
   if request.form.get('star') == 'notstared':
     pass
   elif request.form.get('star') == 'stared':
@@ -58,17 +42,10 @@ def index_post():
 
   return ('', 204)
 
-
-
-
-
 @main.route('/profile')
 @login_required
 def profile():
   return render_template('profile.html', name=current_user.name)
-
-
-
 
 @main.route('/upload')
 @login_required
@@ -77,18 +54,19 @@ def upload():
 
 @main.route('/upload', methods=['POST'])
 def upload_post():
-
   if 'file' not in request.files:
     flash('No file part')
     return redirect(request.url)
+  
   file = request.files['file']
+  
   if file.filename == '':
     flash('No selected file')
     return redirect(request.url)
+  
   if file and allowed_file(file.filename):
     body = file.read()
-    contenttype = magic.from_buffer(body)
-
+    contenttype = magic.from_buffer(body, mime=True)
 
     file_hash = hashlib.sha256(body).hexdigest()
     filename = os.path.join(UPLOAD_FOLDER,file_hash)
@@ -96,70 +74,48 @@ def upload_post():
     open(filename,'wb').write(body)
 
     image = Image(
-            owner_id=current_user.id,
-            hash=file_hash,
-            length = len(body),
-            contentType = contenttype 
-        )
-
+      owner_id=current_user.id,
+      hash=file_hash,
+      length = len(body),
+      contentType = contenttype 
+      )
 
     db.session.add(image)
     db.session.commit()
 
     flash('Uploaded!')
     return redirect(request.url)
-
-
-
-
   
   flash('Please try again, there was a problem')
   return redirect(request.url)
 
-
-
-
-
-
-
-
-
-
 @main.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(main.config[db.session.execute(db.select(Image).order_by(Image.location)).scalars()],
-                               filename)
-
-
+  return send_from_directory(main.config[db.session.execute(db.select(Image).order_by(Image.location)).scalars()],
+                             filename)
 
 @main.route("/getimage")
 def get_img():
-    file_name = ""
-  
-    if (request.args.get("num") != None):
+  file_name = ""
 
-        start = int(request.args.get("num"))
+  if (request.args.get("num") != None):
+    start = int(request.args.get("num"))
 
-        if(start < 0):
-          rem = int((abs(start)+1)%len(file_names))
-          if (rem == 0):
-            rem = len(file_names)
+    if(start < 0):
+      rem = int((abs(start)+1)%len(file_names))
+      if (rem == 0):
+        rem = len(file_names)
 
-          start = len(file_names)-rem
+      start = len(file_names)-rem
 
-        if(start >(len(file_names)-1)):
-          rem = int((abs(start)+1)%len(file_names))
-          if (rem == 0):
-            rem = len(file_names)
-          start = rem-1;
+    if(start >(len(file_names)-1)):
+      rem = int((abs(start)+1)%len(file_names))
+      if (rem == 0):
+        rem = len(file_names)
+      start = rem-1;
+    return str(file_names[start])
+  else:
+    random.shuffle(file_names);
 
-
-        
-        return str(file_names[start])
-
-
-    else:
-      random.shuffle(file_names);
-
-      return str(file_names[0])
+    return str(file_names[0])
 
