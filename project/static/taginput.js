cc12c526e992c0a9c5f3d777660af53ee1e1d5a4
qimcis <153383231+qimcis@users.noreply.github.com>
2024-07-17 15:16:23 +0800

    trying to implement tagify (it's not working yet)

---
+++ project/static/taginput.js
@@ -0,0 +1,25 @@
+import Tagify from '@yaireo/tagify'
+import './styles.scss' 
+
+document.addEventListener("DOMContentLoaded", function() {
+    var input = document.querySelector('input[name=rtl-example]');
+
+    if (input) {
+        new Tagify(input, {
+            whitelist: [
+                { value: "Nude"},
+                { value: "Clothed"},
+                { value: "Male"},
+                { value: "Female"},
+                { value: "Non-Binary"},
+            ],
+            dropdown: {
+                mapValueTo: 'full',
+                classname: 'tagify__dropdown--rtl-example',
+                enabled: 0,
+                RTL: true,
+                escapeHTML: false 
+            }
+        });
+    }
+});
